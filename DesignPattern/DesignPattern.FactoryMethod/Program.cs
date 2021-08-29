7cfb3341e50a53a9edeb5fdb3e410a5cd880406e
beboantonio <beboantonio1@hotmail.com>
2021-08-29 13:20:48 -0300

    add factory method demo

--- DesignPattern/DesignPattern.FactoryMethod/Program.cs
+++ DesignPattern/DesignPattern.FactoryMethod/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using DesignPattern.FactoryMethod.Factory;
 
 namespace DesignPattern.FactoryMethod
 {
+    //// <summary>
+    //// Factory Pattern Demo
+    //// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            VehicleFactory factory = new ConcreteVehicleFactory();
+            IFactory scooter = factory.GetVehicle("Scooter");
+            scooter.Drive(10);
+
+            IFactory bike = factory.GetVehicle("Bike");
+            bike.Drive(20);
+
+            Console.ReadKey();
         }
     }
 }

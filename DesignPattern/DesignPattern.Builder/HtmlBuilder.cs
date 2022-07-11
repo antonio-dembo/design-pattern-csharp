f15e0b881ba066aad01cef69c841e105668cf03a
antonio-dembo <beboantonio1@hotmail.com>
2022-07-11 20:34:48 -0300

    Pushing missing untracked files

---
+++ DesignPattern/DesignPattern.Builder/HtmlBuilder.cs
@@ -0,0 +1,31 @@
+﻿namespace DesignPattern.Builder
+{
+    public class HtmlBuilder
+    {
+        HtmlElement root = new HtmlElement();
+        private string rootName;
+
+        public HtmlBuilder(string rootName)
+        {
+            this.rootName = rootName;
+            root.name = rootName;
+        }
+
+        public HtmlBuilder AddChild(string childName, string childText)
+        {
+            var e = new HtmlElement(childName, childText);
+            root.elements.Add(e);
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return root.ToString();
+        }
+
+        public void Clear()
+        {
+            root = new HtmlElement { name = rootName };
+        }
+    }
+}

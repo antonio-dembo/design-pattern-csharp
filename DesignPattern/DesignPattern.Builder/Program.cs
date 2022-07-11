88a31cae06328c3a564acd3f0879a24d5ba9c94f
antonio-dembo <beboantonio1@hotmail.com>
2022-07-11 19:23:00 -0300

    Life without the Builder pattern

---
+++ DesignPattern/DesignPattern.Builder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace DesignPattern.Builder
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = "hello";
+            var sb = new StringBuilder();
+            sb.Append("<p>");
+            sb.Append("hello");
+            sb.Append("</p>");
+            Console.WriteLine(sb);
+
+            var words = new[] { "hello", "world" };
+            sb.Clear();
+            sb.Append("<ul>");
+            foreach (string word in words)
+            {
+                sb.AppendFormat("<li>{0}</li>", word);
+            }
+            sb.Append("</ul>");
+            Console.WriteLine(sb);
+
+;        }
+    }
+}

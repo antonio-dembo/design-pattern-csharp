587d123fa55a48cbd98e0ba313c7721c54b8ff3b
antonio-dembo <beboantonio1@hotmail.com>
2022-07-11 21:22:46 -0300

    This code fail because PersonInfoBuilder know nothing about WorkAt() method

--- DesignPattern/DesignPattern.Builder.Fluent/Program.cs
+++ DesignPattern/DesignPattern.Builder.Fluent/Program.cs
@@ -2,11 +2,43 @@
 
 namespace DesignPattern.Builder.Fluent
 {
-    class Program
+    public class Person
+    {
+        public string Name;
+        public string Postion;
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Postion)}: {Postion}";
+        }
+    }
+
+    public class PersonInfoBuilder
+    {
+        protected Person Person = new Person();
+
+        public PersonInfoBuilder Called( string name)
+        {
+            Person.Name = name;
+            return this;
+        }
+    }
+
+    public class PersonjobBuilder : PersonInfoBuilder
+    {
+        public PersonjobBuilder WorkAt (string position)
+        {
+            Person.Postion = position;
+            return this;
+        }
+    }
+
+    static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new PersonjobBuilder();
+            //builder.Called("dmitri").WorkAt("dev"); // This will fail if uncomment.
         }
     }
 }
